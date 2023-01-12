289ffaf05e890017d7d2252cb3977dbcc8177998
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-01-12 17:27:44 +0530

    setup for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import VideoDetail  from "./components/VideoDetail";
+import SearchFeed from "./components/SearchFeed";
+import ChannelDetail from "./components/ChannelDetail";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor : "#0000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element= {<Feed/>} />
+          <Route path="/video/:id" element={<VideoDetail/>} />
+          <Route path="/channel/:id" element={<ChannelDetail/>} />
+          <Route path="/search/:searchTerm" element={<SearchFeed/>} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
